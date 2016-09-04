c1c675979517d63b1b2c086f30270e4fb5feaa5a
Ryan Tablada <ryan.tablada@gmail.com>
2016-09-03 23:34:47 -0500

    refactor: changeDirectory

--- app/actions/project-path.js
+++ app/actions/project-path.js
@@ -4,15 +4,21 @@ const { remote: { dialog } } = electron;
 
 export const CHOOSE_PROJECT_PATH = 'CHOOSE_PROJECT_PATH';
 
-export function chooseDirectory() {
-  // Open folder
-  const selection = dialog.showOpenDialog({ properties: ['openDirectory'] });
-  if (selection) {
-    const [projectPath] = selection;
+export function changeDirectory(projectPath) {
+  return {
+    type: CHOOSE_PROJECT_PATH,
+    projectPath
+  };
+}
 
-    return {
-      type: CHOOSE_PROJECT_PATH,
-      projectPath
-    };
-  }
+export function chooseDirectory() {
+  return (dispatch) => {
+  // Open folder
+    const selection = dialog.showOpenDialog({ properties: ['openDirectory'] });
+    if (selection) {
+      const [projectPath] = selection;
+
+      dispatch(changeDirectory(projectPath));
+    }
+  };
 }
