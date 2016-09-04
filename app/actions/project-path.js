b01a64bf3a13b62138704e6277c61b38f0f27146
Ryan Tablada <ryan.tablada@gmail.com>
2016-09-03 23:31:36 -0500

    refactor: Move directory to action

--- app/actions/project-path.js
+++ app/actions/project-path.js
@@ -1,8 +1,18 @@
+import electron from 'electron';
+
+const { remote: { dialog } } = electron;
+
 export const CHOOSE_PROJECT_PATH = 'CHOOSE_PROJECT_PATH';
 
-export function chooseDirectory(projectPath) {
-  return {
-    type: CHOOSE_PROJECT_PATH,
-    projectPath
-  };
+export function chooseDirectory() {
+  // Open folder
+  const selection = dialog.showOpenDialog({ properties: ['openDirectory'] });
+  if (selection) {
+    const [projectPath] = selection;
+
+    return {
+      type: CHOOSE_PROJECT_PATH,
+      projectPath
+    };
+  }
 }
--- app/components/Home.js
+++ app/components/Home.js
@@ -2,31 +2,18 @@ import React, { Component, PropTypes } from 'react';
 import electron from 'electron';
 import styles from './Home.css';
 
-const { remote: { dialog } } = electron;
-
 export default class Home extends Component {
   static propTypes = {
     chooseDirectory: PropTypes.func.isRequired
   };
 
-  askForFolderPath() {
-    const { chooseDirectory } = this.props;
-
-    // Open folder
-    const selection = dialog.showOpenDialog({ properties: ['openDirectory'] });
-    if (selection) {
-      const [directory] = selection;
-
-      return chooseDirectory(directory);
-    }
-  }
-
   render() {
+    const { chooseDirectory } = this.props;
     return (
       <div>
         <div className={styles.container}>
           <h2>Let's Get Started</h2>
-          <button onClick={this.askForFolderPath.bind(this)}>
+          <button onClick={chooseDirectory}>
             Choose File
           </button>
         </div>
