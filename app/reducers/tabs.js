e5ddddf67828398fb259a7d4ba364002eabb1448
Ryan Tablada <ryan.tablada@gmail.com>
2016-09-08 14:12:57 -0500

    fix: no duplicates in tab view

--- app/reducers/tabs.js
+++ app/reducers/tabs.js
@@ -3,7 +3,11 @@ import { ADD_TAB, REMOVE_TAB } from '../actions/tabs';
 export default function counter(state = [], action) {
   switch (action.type) {
     case ADD_TAB:
-      return [...state, action.file];
+      if (state.indexOf(action.file) === -1) {
+        return [...state, action.file];
+      }
+
+      return state;
     case REMOVE_TAB:
       return state.filter((file) => file !== action.file);
     default:
