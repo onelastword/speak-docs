eefdf69890dada2b7e957dd00379e7cd6331c7cc
Ryan Tablada <ryan.tablada@gmail.com>
2016-09-08 13:56:43 -0500

    feat: User can see codemirror editor

--- app/components/Editor.js
+++ app/components/Editor.js
@@ -1,9 +1,16 @@
 import React, { Component, PropTypes } from 'react';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/mode/gfm/gfm';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/handlebars/handlebars';
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/sass/sass';
+
 import Nav from './Editor/Nav';
 import TreeView from './Editor/TreeView';
 import TabView from './Editor/TabView';
 import style from './Editor.css';
-
 
 export default class Editor extends Component {
   static propTypes = {
@@ -26,6 +33,16 @@ export default class Editor extends Component {
       tabs, savedFile, projectPath, currentFile, directoryTree,
     } = this.props;
 
+    const codeMirrorOptions = {
+      lineWrapping: true,
+      lineNumbers: true,
+      mode: 'gfm',
+      viewportMargin: 300,
+      theme: 'ambiance',
+      tabSize: 2,
+      indentWithTabs: false,
+    };
+
     return (
       <div className={style.fullscreen}>
         <Nav projectPath={projectPath} chooseDirectory={chooseDirectory} />
@@ -33,7 +50,12 @@ export default class Editor extends Component {
           <TreeView directoryTree={directoryTree} currentFile={currentFile} addTab={addTab} />
           <div className={style.tabContainer}>
             <TabView tabs={tabs} removeTab={removeTab} />
-            <code><pre>{savedFile}</pre></code>
+            <div className={style.editorPane}>
+              <CodeMirror
+                className={style.editorContainer} value={savedFile}
+                options={codeMirrorOptions}
+              />
+            </div>
           </div>
         </div>
       </div>
